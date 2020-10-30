9c8c65df0232fe31dee047696e20d34bd30b85fe
oanaford <73721534+oanaford@users.noreply.github.com>
2020-10-30 16:07:04 -0600

    code and data

---
+++ wcloud.R
@@ -0,0 +1,67 @@
+BASE_DIR <- getwd()
+setwd(BASE_DIR)
+
+library('RColorBrewer')
+library('wordcloud')
+library('SnowballC')
+library('NLP')
+library('tm')
+
+
+text <- readLines(paste0(BASE_DIR, "/Data/Jobs.txt"))
+text
+
+# Load the data as a corpus
+docs <- Corpus(VectorSource(text))
+
+inspect(docs)
+
+#test transformation
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+#cleaning
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+docs <- tm_map(docs, stemDocument)
+
+#build term-doc matrix
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+
+#generate wc
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+#frequent terms
+findFreqTerms(dtm, lowfreq = 4)
+
+#find associations
+findAssocs(dtm, terms = "colleg", corlimit = 0.3)
+
+#freq of table words
+head(d, 10)
+
+#plot frequencies
+barplot(d[1:10,]$freq, las = 2, names.arg = d[1:10,]$word,
+        col ="lightblue", main ="Most frequent words",
+        ylab = "Word frequencies")
